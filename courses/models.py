e6d20daf7026641d69cdafab8b066f8639168c0c
erfan <eazari.dev@gmail.com>
2025-05-31 13:26:38 +0330

    create custom order field for ordering and add to Module and Content objects

--- courses/models.py
+++ courses/models.py
@@ -6,6 +6,7 @@ from django.db import models
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 
+from .fields import OrderField
 
 class Subject(models.Model):
     title = models.CharField(max_length=200)
@@ -57,10 +58,14 @@ class Module(models.Model):
     
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True)
-    
+    order = OrderField(blank=True, for_fields=['course'])
     
     def __str__(self):
-        return self.title
+        return f'{self.order}. {self.title}'
+    
+    
+    class Meta:
+        ordering = ['order']
     
     
     
@@ -83,7 +88,11 @@ class Content(models.Model):
     
     object_id = models.PositiveIntegerField()
     item = GenericForeignKey('content_type', 'object_id')
+    order = OrderField(blank=True, for_fields=['module'])
     
+    
+    class Meta:
+        ordering = ['order']
     
 # use Model Inheritance to implement polymorphic models
 class ItemBase(models.Model):
