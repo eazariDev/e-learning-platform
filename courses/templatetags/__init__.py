60361eff25ed7a6936fe4822de213d7cf42376f1
erfan <eazari.dev@gmail.com>
2025-06-02 16:16:00 +0330

    managing course modules and their contents using django formsets

---
+++ courses/templatetags/__init__.py
@@ -0,0 +1,1 @@
+# courses/templatetags/__init__.py
---
+++ courses/templatetags/course.py
@@ -0,0 +1,12 @@
+# courses/templatetags/course.py
+
+from django import template
+
+register = template.Library()
+
+@register.filter
+def model_name(obj):
+    try:
+        return obj._meta.model_name
+    except AttributeError:
+        return None
