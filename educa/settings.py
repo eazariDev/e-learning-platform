4e8228f1ff58138087dcc0c7205a8195df01a0f5
erfan <eazari.dev@gmail.com>
2025-06-03 17:31:04 +0330

    implement student registration

--- educa/settings.py
+++ educa/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     
     'courses.apps.CoursesConfig',
+    'students.apps.StudentsConfig',
+    
 ]
 
 MIDDLEWARE = [
--- educa/urls.py
+++ educa/urls.py
@@ -30,6 +30,8 @@ urlpatterns = [
     path('course/', include('courses.urls')),
     
     path('', CourseListView.as_view(), name='course_list'),
+    
+    path('students/', include('students.urls')),
 ]
 
 
